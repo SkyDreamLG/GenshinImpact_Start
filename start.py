61a643a2da572c21f43bf8f26bf5199159faa7ae
SkyDreamLG <1607002411@qq.com>
2023-09-19 23:11:27 +0800

    修正了文件无法写入的bug 精简了部分代码

--- start.py
+++ start.py
@@ -19,8 +19,7 @@ def is_admin():
 
 
 if is_admin():
-    path = ''  # 初始化文件路径变量
-    print('运行本程序前请确保原神图像设置已设置为独占全屏，否则会导致效果不佳')
+    print('运行本程序前请确保原神游戏内图像设置已设置为独占全屏，否则会导致效果不佳')
     input('按回车键继续')
 
     # 检查原神是否已经启动
@@ -33,26 +32,21 @@ if is_admin():
     # 定义设置原神游戏路径功能函数
     def ys_path():
         path = str(input('请输入原神游戏主程序文件绝对路径（不要带有引号）：'))  # 获取用户输入的路径数据并写入变量
+        with open('yuanshen_path.txt', 'w') as path_save:
+            path_save.write(path)
         return path
-
-
-    def path_set():
-        with open('yuanshen_path.txt', 'w') as file_object:
-            file_object.write(path)
-
 
     # 尝试读取路径记录文件
     try:
         # 打开路径记录文件
-        with open('yuanshen_path.txt') as file_object:
-            path_0 = file_object.read()  # 读取文件
+        with open('yuanshen_path.txt') as path_save:
+            path_0 = path_save.read()  # 读取文件
             path = path_0.rstrip()  # 去除读取路径结尾空行并将路径数据写入对应变量
         try:
             user_choose = str(input('是否重新设置原神主程序游戏文件绝对路径？（Y/N）：'))  # 获取用户意愿
             user_choose = user_choose.lower()  # 将用户输入转化为小写
             if user_choose == 'y':  # 若用户输入为是，则执行重置函数
                 ys_path()
-                path_set()
             else:  # 若用户输入为否，则跳过
                 print('已跳过')
                 input('按回车键继续')
@@ -62,7 +56,6 @@ if is_admin():
     except:
         print('错误：原神游戏主程序文件路径未指定，请指定原神游戏主程序文件位置')
         ys_path()
-        path_set()
 
     # 获取屏幕分辨率
     hDC = win32gui.GetDC(0)
@@ -84,9 +77,9 @@ if is_admin():
         # 判断是否满足启动条件
         if white_percentage >= 90:
             break
-
+    print(path)
     # 原神，启动！
-    os.startfile(r"E:\Genshin Impact Game\YuanShen.exe")
+    os.startfile(path)
 
     print('原神，启动！！！！')
 
